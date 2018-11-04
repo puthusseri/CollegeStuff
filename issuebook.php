89cbe8636adca6514ca6cc7017b44a5ca2740f83
puthusseri <vyshakputhusseri@gmail.com>
2018-11-04 04:58:43 -0800

    issue page was made

---
+++ issuebook.php
@@ -0,0 +1,14 @@
+
+<div>
+    <form method = POST>
+        <label for="bookname">Book Name</label>
+        <input type="text" id="bookname" name="bookname" placeholder="Enter the book name">
+
+        <label for="id">Book ID</label>
+        <input type="text" id="id" name="id" placeholder="Enter the Book ID">
+        <center>
+            <input type="submit" value="Submit">
+            <input type = reset value = Reset>
+        </center>
+    </form>
+</div>
