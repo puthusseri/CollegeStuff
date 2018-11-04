09255fcd06301db5f687d132aaccf845a57a5580
puthusseri <vyshakputhusseri@gmail.com>
2018-11-04 03:53:46 -0800

    add books

---
+++ addbook.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" type="text/css" href="style_for_addbook.css">
+
+<div>
+    <form method = POST>
+        <label for="bookname">Book Name</label>
+        <input type="text" id="bookname" name="firstnbooknameame" placeholder="Enter the book name">
+
+        <label for="author">Author Name</label>
+        <input type="text" id="lname" name="lname" placeholder="Enter the author name">
+        <center>
+            <input type="submit" value="Submit">
+            <input type = reset value = Reset>
+        </center>
+    </form>
+</div>
